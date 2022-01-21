f173cdd90ac87774ec433c724a439ca5fc4d1697
Erwin-Feld <erwinfeld.dev@gmail.com>
2022-01-21 11:38:49 +0100

    added swift reactor comp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ReactorManagerComponent } from './components/reactor-manager/reactor-manager.component';
 import { SteadyReactorComponent } from './components/steady-reactor/steady-reactor.component';
+import { SwiftReactorComponent } from './components/swift-reactor/swift-reactor.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ReactorManagerComponent,
-    SteadyReactorComponent
+    SteadyReactorComponent,
+    SwiftReactorComponent
   ],
   imports: [
     BrowserModule
