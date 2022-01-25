6899bca15cb9a4fa8eb832b574bade702c6a3386
Erwin-Feld <erwinfeld.dev@gmail.com>
2022-01-25 12:21:09 +0100

    added NgChartsModule to global app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { NgChartsModule } from 'ng2-charts';
+
+
 import { AppComponent } from './app.component';
 import { ReactorManagerComponent } from './components/reactor-manager/reactor-manager.component';
 import { SteadyReactorComponent } from './components/steady-reactor/steady-reactor.component';
 import { SwiftReactorComponent } from './components/swift-reactor/swift-reactor.component';
 import { LambdaReactorComponent } from './components/lambda-reactor/lambda-reactor.component';
+import { GlobalOutputLineChartComponent } from './components/global-output-line-chart/global-output-line-chart.component';
 
 @NgModule({
   declarations: [
@@ -13,10 +17,12 @@ import { LambdaReactorComponent } from './components/lambda-reactor/lambda-react
     ReactorManagerComponent,
     SteadyReactorComponent,
     SwiftReactorComponent,
-    LambdaReactorComponent
+    LambdaReactorComponent,
+    GlobalOutputLineChartComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgChartsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
